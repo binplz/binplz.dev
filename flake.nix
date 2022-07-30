8593b3090db7c1cf51de790590dc0e75868d23d6
Jonas Carpay <jonascarpay@gmail.com>
2022-07-30 23:57:09 +0900

    Server scaffolding

--- flake.nix
+++ flake.nix
@@ -1,24 +1,43 @@
 {
-  description = "binplz";
+  description = "binplz-server";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+  inputs.nixpkgs = { };
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = inputs:
     let
-      overlay = import nix/overlay.nix;
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              binplz-server = hfinal.callCabal2nix "binplz-server" ./. { };
+            };
+        };
+        binplz-server = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.binplz-server;
+        binplz-docs = final.callPackage ../docs { };
+      };
       perSystem = system:
         let
           pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
         in
         {
-          # defaultPackage = pkgs.purenix;
-          # packages.purenix = pkgs.purenix;
-
-          packages.docs = pkgs.binplz-docs;
-
-          # devShell = pkgs.hacking-on-purenix-shell;
-          # devShells = { };
+          devShell = hspkgs.shellFor {
+            withHoogle = true;
+            packages = p: [ p.binplz-server ];
+            buildInputs = [
+              hspkgs.cabal-install
+              hspkgs.haskell-language-server
+              hspkgs.hlint
+              hspkgs.ormolu
+              pkgs.bashInteractive
+            ];
+          };
+          defaultPackage = pkgs.binplz-server;
+          packages = {
+            docs = pkgs.binplz-docs;
+            server = pkgs.binplz-server;
+          };
         };
     in
     { inherit overlay; } // inputs.flake-utils.lib.eachDefaultSystem perSystem;
