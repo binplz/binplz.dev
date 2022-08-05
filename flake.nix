3f8fac685edc3d8258b24ac2a650022320b92627
Jonas Carpay <jonascarpay@gmail.com>
2022-08-05 12:01:21 +0900

    flake: pin nixpkgs to release-22.05 (again)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "binplz-server";
 
-  inputs.nixpkgs = { };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = inputs:
