ccf47a6d94763619ad394142aea6266a06a9ec96
Viktor Kronvall <viktor.kronvall@gmail.com>
2022-09-10 00:41:54 +0900

    Print a prefix for each line in tee

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -14,12 +14,14 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Except (ExceptT (..), runExceptT, withExceptT)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy as Lazy
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Data.ByteString.Lazy.UTF8 as UTF8
 import Data.Coerce (coerce)
 import Data.Either (isLeft)
 import Data.Maybe (fromMaybe)
+import Data.String (fromString)
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromField (FromField (..))
 import Database.SQLite.Simple.ToField (ToField (..))
@@ -157,48 +159,61 @@ buildTriplet appDB trip@(BinaryTriplet bin pkg sys) = withApplicationDB appDB $ 
 
 -- | Write the data from the input 'Handle' to the output 'Handle' and collect
 -- the read bytes into a lazy bytestring.
-tee' :: Handle -> Handle -> IO Lazy.ByteString
-tee' input output = do
+--
+-- A prefix is added to each line to support giving extra context to which process is running like
+-- request-42> Waiting for locks on /nix/store/...
+teePrefix :: ByteString -> Handle -> Handle -> IO Lazy.ByteString
+teePrefix prefix input output = do
   str <- Lazy.hGetContents input
-  result <-
-    BSL.fromChunks
-      <$> traverse (\chunk -> chunk <$ BS.hPut output chunk) (Lazy.toChunks str)
+  result <- BSL.fromChunks . concat <$> traverse teeLine (BSL.lines str)
   IO.hClose input
   pure result
+  where
+    teeLine line = do
+      BS.hPut output prefix
+      -- add a trailing newline to the line
+      let chunks = BSL.toChunks line <> [BS8.singleton '\n']
+      traverse (\chunk -> chunk <$ BS.hPut output chunk) chunks
 
-nixBuild :: BinaryTriplet -> IO (Either String BSL.ByteString)
-nixBuild (BinaryTriplet bin pkg sys) = runExceptT $ do
-  -- TODO make sure we can't get any XSS-like shenanigans
+streamCommand :: (Handle -> IO Lazy.ByteString) -> (Handle -> IO Lazy.ByteString) -> FilePath -> [String] -> ExceptT String IO (ExitCode, BSL.ByteString, BSL.ByteString)
+streamCommand onStdout onStderr cmd args = do
   let process =
-        ( Proc.proc
-            "nix"
-            ( ["build", "nixpkgs#legacyPackages." <> show sys <> ".pkgsStatic." <> unPackageName pkg]
-                -- Don't make a result symlink, just print it to stdout
-                <> ["--no-link", "--print-out-paths"]
-                -- Print build logs to stderr
-                <> ["--print-build-logs"]
-                -- Only allow access to files from NIX_PATH. Derivations can't read files like `/etc/shadow`.
-                <> ["--option", "restrict-eval", "true"]
-                -- Disallow IFD.  Probably not necessary, since Nixpkgs doesn't contain derivations that do IFD, but better to be on the safe side.
-                <> ["--option", "allow-import-from-derivation", "false"]
-                -- Make sure the sandbox is always used.
-                <> ["--option", "sandbox-fallback", "false"]
-            )
-        )
+        (Proc.proc cmd args)
           { Proc.std_in = Proc.CreatePipe,
             Proc.std_out = Proc.CreatePipe,
             Proc.std_err = Proc.CreatePipe
           }
-  (exit, stdout, stderr) <- ExceptT $
+  ExceptT $
     liftIO $
       Proc.withCreateProcess process $ \minh mouth merrh processHandle -> do
         case (minh, mouth, merrh) of
           (Just inh, Just outh, Just errh) -> do
             IO.hClose inh
-            (stdout, stderr) <- concurrently (tee' outh IO.stdout) (tee' errh IO.stderr)
+            (stdout, stderr) <- concurrently (onStdout outh) (onStderr errh)
             exit <- Proc.waitForProcess processHandle
             pure $ pure (exit, stdout, stderr)
           _ -> pure $ throwError "couldn't read process output"
+
+nixBuild :: BinaryTriplet -> IO (Either String BSL.ByteString)
+nixBuild (BinaryTriplet bin pkg sys) = runExceptT $ do
+  -- TODO make sure we can't get any XSS-like shenanigans
+  (exit, stdout, stderr) <-
+    streamCommand
+      (\out -> teePrefix (fromString "out> ") out IO.stdout)
+      (\err -> teePrefix (fromString "err> ") err IO.stderr)
+      "nix"
+      ( ["build", "nixpkgs#legacyPackages." <> show sys <> ".pkgsStatic." <> unPackageName pkg]
+          -- Don't make a result symlink, just print it to stdout
+          <> ["--no-link", "--print-out-paths"]
+          -- Print build logs to stderr
+          <> ["--print-build-logs"]
+          -- Only allow access to files from NIX_PATH. Derivations can't read files like `/etc/shadow`.
+          <> ["--option", "restrict-eval", "true"]
+          -- Disallow IFD.  Probably not necessary, since Nixpkgs doesn't contain derivations that do IFD, but better to be on the safe side.
+          <> ["--option", "allow-import-from-derivation", "false"]
+          -- Make sure the sandbox is always used.
+          <> ["--option", "sandbox-fallback", "false"]
+      )
   liftIO . putStrLn $
     unlines
       [ "Finished a Nix build call",
